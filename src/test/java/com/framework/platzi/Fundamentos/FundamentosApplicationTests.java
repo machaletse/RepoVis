6f05cfecff218eebee1c5c64704334670acb629a
IsaCamber <e.isacabe@gmail.com>
2021-10-02 15:39:28 -0500

    Second commit

---
+++ src/test/java/com/framework/platzi/Fundamentos/FundamentosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.framework.platzi.Fundamentos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FundamentosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
